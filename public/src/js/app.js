4ae716e5725ffb9e6117a32e55766b2160caa793
Mihir <mihir.mg@somaiya.edu>
2018-12-21 21:12:29 +0530

    minor change in feed,js

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,22 +1,23 @@
-var deferredPrompt; 
+var deferredPrompt;
 
-if(!window.Promise){
-	window.Promise = Promise;
+if (!window.Promise) {
+  window.Promise = Promise;
 }
 
-if('serviceWorker' in navigator){
-	navigator.serviceWorker
-	 	.register('/sw.js')		//.register('/sw.js', {scope: '/help/'}) to override the default scope and set it to only help folder, but only inside the default one, not outside
-	 	.then(function(){
-	 		console.log('Service worker registered');
-	 	}).catch(function(){
-	 		console.log('Service worker could not be registered');
-	 	});
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/sw.js')
+    .then(function () {
+      console.log('Service worker registered!');
+    })
+    .catch(function(err) {
+      console.log(err);
+    });
 }
 
-window.addEventListener('beforeinstallprompt', function(event){
-	console.log('beforeinstallprompt fired');
-	event.preventDefault();
-	deferredPrompt = event;
-	return false;
-}); 
+window.addEventListener('beforeinstallprompt', function(event) {
+  console.log('beforeinstallprompt fired');
+  event.preventDefault();
+  deferredPrompt = event;
+  return false;
+});
